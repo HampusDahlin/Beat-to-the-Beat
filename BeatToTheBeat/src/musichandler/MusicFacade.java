ca3ea4e461cef6927297ef07e805796225ab2ef7
Hampus Dahlin <hampus.dahlin@gmail.com>
2014-03-25 20:15:47 +0100

    additional methods added to the musichandler package

--- BeatToTheBeat/src/musichandler/MusicFacade.java
+++ BeatToTheBeat/src/musichandler/MusicFacade.java
@@ -1,5 +1,31 @@
 package musichandler;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * @author Hampus Dahlin
+ * @version 0.0.1
+ */
 public class MusicFacade {
+	
+	/**
+	 * 
+	 * @param s
+	 * @return
+	 */
+	public List<Integer> timerGenerator(Song s){
+		List<Integer> timers = new ArrayList<Integer>();
+		
+		
+		return timers;
+	}
+	
+	
+	
+	public void changeDist(){
+		
+	}
 	
 }
