accfc3d294607f01e33237459117fe31fb1c1104
Malin <malin.thelin12@gmail.com>
2014-03-26 10:43:58 +0100

    Started on BackgroundLayer class

--- BeatToTheBeat/src/enviroment/BackgroundLayer.java
+++ BeatToTheBeat/src/enviroment/BackgroundLayer.java
@@ -1,5 +1,8 @@
 package enviroment;
 
+import java.awt.Image;
+import java.util.List;
+
 /**
  * 
  * @author Hampus Dahlin
@@ -7,5 +10,34 @@ package enviroment;
  *
  */
 public class BackgroundLayer {
+	
+	private int speed;
+	private Image sprite;
+	private List<BackgroundObject> objects;
+	
+	public BackgroundLayer(int speed,Image sprite){
+		this.speed = speed;
+		this.sprite = sprite;
+	}
+	
+	public int getSpeed(){
+		return speed;
+	}
+	
+	public void setSpeed(int speed){
+		this.speed = speed;
+	}
+	
+	public Image getSprite(){
+		return sprite;
+	}
+	
+	public void addBackgroundObject(BackgroundObject object){
+		objects.add(object);
+	}
+	
+	public void removeBackgroundObject(int bgIndex){
+		objects.remove(bgIndex);
+	}
 
 }
