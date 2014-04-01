213241db55545352b9f6d1b5ce36209cb22ef815
Pontus Eriksson <pontus_eriksson@live.com>
2014-04-01 11:32:10 +0200

    Added initial "levels" package.

---
+++ BeatToTheBeat/src/levels/BttBLevel.java
@@ -0,0 +1,48 @@
+package levels;
+
+import enviroment.Background;
+
+/** 
+* Levels(or stages) for the game "Beat to the Beat".
+* <p> Contains everything needed for a level.
+* @author  Pontus "Bondi" Eriksson
+* @group 14
+* @date 01-04-14
+*/ 
+
+public class BttBLevel implements ILevel {
+	private final String fileName;
+	private final String songName;
+	private final Background background;
+	private final long spawnTimes[]; //Array containing the spawntimes for NPCs.
+	
+	//Loading from saved file.
+	public BttBLevel(String fileName, String songName, Background background, long spawnTimes[]) {
+		this.fileName = fileName;
+		this.songName = songName;
+		this.background = background;
+		this.spawnTimes = spawnTimes;
+	}
+	
+	//Creating a new level from a song.
+	/*public BttBLevel(String fileName, String songName, Background background) {
+		
+	}*/
+	
+	public String getFileName() {
+		return fileName;
+	}
+	
+	public String getSongName() {
+		return songName;
+	}
+	
+	public Background getBackground() {
+		return background;
+	}
+	
+	public long[] getSpawnTimes() {
+		return spawnTimes;
+	}
+
+} // end BttBLevel
---
+++ BeatToTheBeat/src/levels/ILevel.java
@@ -0,0 +1,11 @@
+package levels;
+
+/** 
+* Interface for Levels(or stages) for any game.
+* <p> Contains everything needed for a level.
+* @author  Pontus "Bondi" Eriksson
+* @group 14
+* @date 01-04-14
+*/ 
+
+public interface ILevel {}
