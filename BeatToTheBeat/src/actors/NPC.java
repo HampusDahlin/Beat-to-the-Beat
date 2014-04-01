b44e45fb95c09280610c127003b4519e77cdc164
Björn Hedström <hebjorn@student.chalmers.se>
2014-04-01 10:47:17 +0200

    Adding some small unfinished methods to the Actor package

--- BeatToTheBeat/src/actors/NPC.java
+++ BeatToTheBeat/src/actors/NPC.java
@@ -3,29 +3,25 @@ package actors;
 import java.awt.Image;
 import java.awt.Point;
 
+import controller.CharacterControl;
+
 public class NPC extends Actor implements support.Movable{
-	private int speed;
-	private int dmg;
-	
+
+
 	public NPC(Point position, Image sprite){
 		setHealth(1);
 		setPosition(position);
 		setSprite(sprite);
 	}
 	
-	public void setSpeed(int newSpeed){
-		speed = newSpeed;
+	public void attack(){
+		if(canHit()){
+			dealDmg(/*CharacterControls PC variabel*/);
+		}
+		death();
 	}
 	
-	public int getSpeed(){
-		return speed;
-	}
-	
-	public void setDmg(int newDmg){
-		dmg = newDmg;
-	}
-	
-	public int getDmg(){
-		return dmg;
+	public void death(){
+		CharacterControl.removeCharacter(this);
 	}
 }
