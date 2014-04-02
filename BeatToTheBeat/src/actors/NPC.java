05223c86306cbc267400da1ef01ce1529544343e
Pontus Eriksson <pontus_eriksson@live.com>
2014-04-02 13:07:13 +0200

    Finished constructor for NPC.

--- BeatToTheBeat/src/actors/NPC.java
+++ BeatToTheBeat/src/actors/NPC.java
@@ -6,11 +6,13 @@ import javax.swing.Icon;
 
 public class NPC extends Actor implements support.Movable{
 
-
 	public NPC(Point position, Icon sprite){
 		setHealth(1);
 		setPosition(position);
 		setSprite(sprite);
+		setDmg(1);
+		setSpeed(new Point(100,0));
+		setRange(1);
 	}
 	
 }
