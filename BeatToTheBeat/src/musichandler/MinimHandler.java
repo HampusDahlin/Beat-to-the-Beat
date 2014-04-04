27b14beb008d56ee697722b93c73eefb7319891e
Pontus Eriksson <pontus_eriksson@live.com>
2014-04-04 13:12:59 +0200

    Added Minim-package (BeatDetect).

---
+++ BeatToTheBeat/src/musichandler/MinimHandler.java
@@ -0,0 +1,21 @@
+package musichandler;
+
+import ddf.minim.*;
+
+public class MinimHandler {
+	Minim minim;
+	AudioPlayer player;
+	AudioInput input;
+
+	void setup() {
+		//size(100, 100);
+
+		minim = new Minim(this);
+		player = minim.loadFile("song.mp3");
+		input = minim.getLineIn();
+	}
+
+	void draw() {
+		// do what you do
+	}
+}
