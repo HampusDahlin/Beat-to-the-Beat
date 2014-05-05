e64a7c2e7722bc233b9af17f8d1cd1fc3ef9d607
Björn Hedström <hebjorn@student.chalmers.se>
2014-05-05 11:45:51 +0200

    Refactoring code in the musicpackage

--- BeatToTheBeat/src/musichandler/Analyzer.java
+++ BeatToTheBeat/src/musichandler/Analyzer.java
@@ -1,16 +1,11 @@
 package musichandler;
 
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.List;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.Timer;
 
 import ddf.minim.*;
@@ -27,13 +22,10 @@ public class Analyzer implements ActionListener {
 	private String songName;
 	private Long currentTime; //in milliseconds 
 	private ArrayList<Long> beatList;
-	private int sensitivity;
-	private Song song;
+	private final int timerCd;
 	
 	public Analyzer(Song song, int sensitivity) {
-		this.song = song;
-		
-		this.sensitivity = sensitivity;
+		timerCd = 20;
 		beatList = new ArrayList<Long>();
 		BUFFERSIZE = 512;
 		songName = "songs\\" + song.getFilename();
@@ -41,7 +33,7 @@ public class Analyzer implements ActionListener {
 		detective = new BeatDetect();
 		detective.setSensitivity(sensitivity);
 		currentTime = new Long(0);
-		beatCheck = new Timer(20, this);
+		beatCheck = new Timer(timerCd, this);
 		beatCheck.start();
 	}
 
@@ -87,34 +79,23 @@ public class Analyzer implements ActionListener {
 	public ArrayList<Long> getBeatList() {
 		return beatList;
 	}
+	
+	public void stopAnalyzer() {
+		player.pause();
+		beatCheck.stop();
+	}
 
 	public void actionPerformed(ActionEvent e) {
 		// getting the buffer for current time in song
 		detective.detect(player.mix);
-		currentTime++;
+		currentTime = currentTime + timerCd;
 		
 		if (detective.isOnset()) {
 			addToBeatList(currentTime);
 		} else if(beatList != null && beatList.size() != 0 &&
 				currentTime - beatList.get(beatList.size() - 1) > 3000) {
-					player.pause();
-					beatCheck.stop();
+				stopAnalyzer();
 		}
 	}
-	//can only give a resonable difficulty scale if called after analyzis is complete.
-	public String difficulty() {
-		double averageBeatsPerSecond = beatNr / (currentTime / 1000);
-		
-		if(beatList.size() > 1) {
-			Long highestTimeBetweenBeats = beatList.get(1) - beatList.get(0);
-			Long lowestTimeBetweenBeats = highestTimeBetweenBeats;
-			for(int i = 1; i < beatList.size(); i++) {
-				if(highestTimeBetweenBeats < (beatList.get(i + 1) - beatList.get(i))) {
-					highestTimeBetweenBeats = beatList.get(i + 1) - beatList.get(i);
-				} else if(lowestTimeBetweenBeats > (beatList.get(i + 1) - beatList.get(i))) {
-					lowestTimeBetweenBeats = beatList.get(i + 1) - beatList.get(i);
-				}
-			}
-		} 
-	}
+	
 }
