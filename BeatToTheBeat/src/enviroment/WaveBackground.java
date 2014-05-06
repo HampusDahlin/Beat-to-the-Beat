e365615c25153ae1b7e65a96a6239abe429d17bb
Hampus Dahlin <hampus.dahlin@gmail.com>
2014-05-06 14:42:26 +0200

    Removed all but the dynamic WaveBackground.

--- BeatToTheBeat/src/enviroment/WaveBackground.java
+++ BeatToTheBeat/src/enviroment/WaveBackground.java
@@ -1,11 +1,25 @@
 package enviroment;
 
-public class WaveBackground implements IBackground{
+import java.awt.Graphics;
 
-	@Override
-	public void update() {
-		// TODO Auto-generated method stub
-		
+import javax.swing.JPanel;
+
+public class WaveBackground extends JPanel{
+
+	Graphics g;
+	public WaveBackground(){
+	}
+	
+	
+	public void updateBackground(float[][] soundwave) {
+		for(int i = 0; i < 511; i++) {
+			g.drawLine(i, (int) (50 + soundwave[0][i]*50), i+1, (int) (50 + soundwave[0][i+1]*50));
+			g.drawLine(i, (int) (150 + soundwave[1][i]*50), i+1, (int) (150 + soundwave[1][i+1]*50));
+		}
+	}
+
+	public JPanel getBackgroundPanel() {
+		return this;
 	}
 
 }
