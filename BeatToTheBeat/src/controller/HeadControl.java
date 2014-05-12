7fdb2c6db63177fbaa40a68778eebb298fc5d381
Pontus Eriksson <pontus_eriksson@live.com>
2014-05-12 12:16:06 +0200

    Revert "did some shit"
    This reverts commit 30026a76caa9e403883e5bcf242c0ef5f8576dfc.

--- BeatToTheBeat/src/controller/HeadControl.java
+++ BeatToTheBeat/src/controller/HeadControl.java
@@ -34,15 +34,14 @@ public class HeadControl implements ActionListener {
 	//private JFrame mainFrame;
 	
 	public HeadControl(JFrame mainFrame) {
-		this.mainFrame = mainFrame;
 		actorControl = new ActorControl();
 		enviromentControl = new EnviromentControl(new WaveBackground(), musicControl.getAnalyzer());
 		musicControl = new MusicControl();
 		time = new Timer(10, this);
 		uiControl = new UIControl(mainFrame);
 		mainPanel = new CardPanel(musicControl.getSongList());
-		this.mainFrame.add(mainPanel);
-		this.mainFrame.setVisible(true);
+		mainFrame.add(mainPanel);
+		mainFrame.setVisible(true);
 	}
 	
 	public void startGame(int songIndex) {
