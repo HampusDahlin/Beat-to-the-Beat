7962222d033e54c6e49e890782cfa8d688da4ba9
Björn Hedström <hebjorn@student.chalmers.se>
2014-05-13 13:56:27 +0200

    un-nestled the HeadControl from the MainFrame

--- BeatToTheBeat/src/gui/MainFrame.java
+++ BeatToTheBeat/src/gui/MainFrame.java
@@ -10,22 +10,13 @@ import controller.HeadControl;
 /**
  * 
  * @author Hampus Dahlin
- *
+ * 
  */
-public class MainFrame extends JFrame{
-	
-	private HeadControl hc;
+public class MainFrame extends JFrame {
 	
 	public MainFrame() {
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setSize(914, 600);
-		this.setLayout(new CardLayout());
-		hc = new HeadControl(this);
-		
-		//this.setVisible(true);
 	}
-	
-	public HeadControl getHeadControl(){
-		return hc;
-	}
+
 }
