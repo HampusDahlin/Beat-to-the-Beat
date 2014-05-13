d1f602165193c9d9e04ccb3e8f7f71d42c822da9
Björn Hedström <hebjorn@student.chalmers.se>
2014-05-13 14:36:01 +0200

    fixing a little problem

--- BeatToTheBeat/src/controller/Main.java
+++ BeatToTheBeat/src/controller/Main.java
@@ -3,7 +3,9 @@ package controller;
 import gui.MainFrame;
 
 public class Main {
-	public static void main(String[] args){
+	
+	public static void main(String[] args) {
 		MainFrame frame = new MainFrame();
+		HeadControl hc = new HeadControl(frame);
 	}
 }
