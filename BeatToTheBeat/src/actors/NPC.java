3bdb83b4bfa6683a9da9028246386400f3678651
Pontus Eriksson <pontus_eriksson@live.com>
2014-05-14 11:07:35 +0200

    NPC takes listener and adds it instantly.

--- BeatToTheBeat/src/actors/NPC.java
+++ BeatToTheBeat/src/actors/NPC.java
@@ -1,18 +1,20 @@
 package actors;
 
 import java.awt.Point;
+import java.beans.PropertyChangeListener;
 
 import javax.swing.ImageIcon;
 
 public class NPC extends Actor implements support.Movable{
 
-	public NPC(Point position, ImageIcon sprite){
+	public NPC(Point position, ImageIcon sprite, PropertyChangeListener listener) {
 		super(sprite, new Point(1,0)); //speed sideways, up
 		setHealth(1);
 		setPosition(position);
 		setDmg(1);
 		setRange(1);
-		pcs.firePropertyChange("newNPC", false, getPosition());
+		pcs.addPropertyChangeListener(listener);
+		pcs.firePropertyChange("newNPC", null, getPosition());
 	}
 	
 }
