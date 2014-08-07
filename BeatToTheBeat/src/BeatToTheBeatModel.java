d2cf48dbd961d8d31a3c3eb9244d1b0488660604
Pontus Eriksson <pontus_eriksson@live.com>
2014-08-07 17:07:39 +0200

    Model created.

---
+++ BeatToTheBeat/src/BeatToTheBeatModel.java
@@ -0,0 +1,40 @@
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+
+import powerup.ExtraLife;
+import powerup.Invincible;
+import powerup.Powerup;
+import powerup.Regen;
+import actors.NPC;
+import actors.PC;
+
+
+public class BeatToTheBeatModel {
+	private PC player;
+	private List<NPC> npcList;
+	private List<Powerup> powerups;
+	
+	public BeatToTheBeatModel() {
+		player = new PC(new Point(450, 0));
+		player.setHealth(player.getMaxHealth()); //setting health after so player fires event to gamepanel		
+		npcList = new ArrayList<NPC>();
+		powerups = new ArrayList<Powerup>();
+		addPowerups();
+	}
+	
+	private void addPowerups() {
+		powerups.add(new Regen());
+		powerups.add(new Invincible());
+		powerups.add(new ExtraLife());
+	}
+	
+	public PC getPlayer() {
+		return player;
+	}
+	
+	public List<NPC> getNpcList() {
+		return npcList;
+	}
+	
+}
