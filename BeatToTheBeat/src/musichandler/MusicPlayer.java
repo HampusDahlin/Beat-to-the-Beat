7a8414e1a4a1259db8ab9eeb07763dbdb5fc7be2
Pontus Eriksson <pontus_eriksson@live.com>
2014-08-10 14:01:39 +0200

    comment

--- BeatToTheBeat/src/musichandler/MusicPlayer.java
+++ BeatToTheBeat/src/musichandler/MusicPlayer.java
@@ -6,7 +6,7 @@ import java.io.InputStream;
 import ddf.minim.AudioPlayer;
 import ddf.minim.Minim;
 
-public class MusicPlayer {
+public class MusicPlayer { //needs to be public for minim
 	private Song song;
 	private AudioPlayer player;
 	private Minim minim;
